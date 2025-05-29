d004eeaf0c27abb6a5ea7dbb8576162342ed97b9
BioTomateDE <latuskati@gmail.com>
2025-05-29 12:56:29 +0200

    429 dont skip message, adjusted sleep, done message

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn main() -> Result<(), String> {
 
     let channels: HashMap<u64, Vec<u64>> = extract_message_ids(&messages_directory)?;
     println!("{}", format!("Got {} messages in {} channels.\n", count_messages(&channels), channels.len()).bright_purple());
-    
+
     let client: Client = Client::builder()
         .user_agent("Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
         .build()
@@ -38,18 +38,23 @@ fn main() -> Result<(), String> {
 
     for (channel_id, message_ids) in &channels {
         for message_id in message_ids {
-            println!("Redacting message {} in channel {}.", message_id.to_string().yellow(), channel_id.to_string().yellow());
-            let redacted_message: String = generate_redacted();
-            match edit_message(&client, &token, *channel_id, *message_id, &redacted_message) {
-                Ok(_) => {}
-                Err((e, retry_after)) => {
-                    println!("{}", format!("Error while editing message: {e}").red());
-                    sleep(Duration::from_secs_f64(retry_after * 1.2));
-                }
-            };
-            sleep(Duration::from_millis(3000));
+            loop {
+                sleep(Duration::from_millis(5500));
+                println!("Redacting message {} in channel {}.", message_id.to_string().yellow(), channel_id.to_string().yellow());
+                let redacted_message: String = generate_redacted();
+                match edit_message(&client, &token, *channel_id, *message_id, &redacted_message) {
+                    Ok(_) => break,
+                    Err((e, retry_after)) => {
+                        println!("{}", format!("Error while editing message: {e}").red());
+                        if retry_after == 0.0 { break }
+                        sleep(Duration::from_secs_f64(retry_after * 1.2));
+                    }
+                };
+            }
         }
     }
+    
+    println!("{}", "\nDone!".bright_green());
 
     Ok(())
 }
