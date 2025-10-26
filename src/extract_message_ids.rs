ebf7eb85490a6b56dea17b14d6a7de4990fb7534
Linus <schlafhase190@gmail.com>
2025-10-26 15:31:23 +0100

    forgor to uncomment and now add the json thing with steall allll the data 😋

--- src/extract_message_ids.rs
+++ src/extract_message_ids.rs
@@ -1,49 +1,60 @@
+use serde::Deserialize;
 use std::collections::HashMap;
 use std::fs::{DirEntry, ReadDir};
 use std::path::PathBuf;
 use std::str::Chars;
-use serde::Deserialize;
 
 #[derive(Deserialize)]
-#[allow(non_snake_case)]    // the field names have to match the one's in discord's json
-struct MessageExportJson {
-    ID: u64,
+#[allow(non_snake_case)] // the field names have to match the one's in discord's json
+pub struct Message {
+    pub ID: u64,
+    pub Timestamp: String,
+    pub Contents: String,
 }
 
-pub fn extract_message_ids(directory: &PathBuf) -> Result<HashMap<u64, Vec<u64>>, String> {
-    const FOLDER_NAME_ERR_MSG: &str = "Names of subfolders should be formatted like `c136132671724532136`!";
-    
-    let entries: ReadDir = directory.read_dir()
+pub fn extract_messages(directory: &PathBuf) -> Result<HashMap<u64, Vec<Message>>, String> {
+    const FOLDER_NAME_ERR_MSG: &str =
+        "Names of subfolders should be formatted like `c136132671724532136`!";
+
+    let entries: ReadDir = directory
+        .read_dir()
         .map_err(|e| format!("Could not get children of directory {directory:?}: {e}"))?;
 
-    let mut channels: HashMap<u64, Vec<u64>> = HashMap::new();
+    let mut channels: HashMap<u64, Vec<Message>> = HashMap::new();
 
     for entry in entries {
-        let entry: DirEntry = entry.map_err(|e| format!("Could not get child of directory: {e}"))?;
+        let entry: DirEntry =
+            entry.map_err(|e| format!("Could not get child of directory: {e}"))?;
         let mut path: PathBuf = entry.path();
-        if !path.is_dir() { continue }
+        if !path.is_dir() {
+            continue;
+        }
 
         path.push("messages.json");
         let file_content: String = std::fs::read_to_string(&path)
             .map_err(|e| format!("Could not read file {path:?}: {e}"))?;
-        let messages: Vec<MessageExportJson> = serde_json::from_str(&file_content)
+        let messages: Vec<Message> = serde_json::from_str(&file_content)
             .map_err(|e| format!("Could not get JSON from messages file {path:?}: {e}"))?;
-        let message_ids: Vec<u64> = messages.iter().map(|i| i.ID).collect();
 
         let folder_name = entry.file_name();
-        let folder_name: &str = folder_name.to_str()
-            .ok_or_else(|| format!("Could not convert file name into UTF-8 string: {:?}", folder_name))?;
+        let folder_name: &str = folder_name.to_str().ok_or_else(|| {
+            format!(
+                "Could not convert file name into UTF-8 string: {:?}",
+                folder_name
+            )
+        })?;
         let mut folder_name_chars: Chars = folder_name.chars();
         if folder_name_chars.next().unwrap() != 'c' {
-            return Err(format!("{FOLDER_NAME_ERR_MSG} Folder name does not start with 'c': {folder_name}"))
+            return Err(format!(
+                "{FOLDER_NAME_ERR_MSG} Folder name does not start with 'c': {folder_name}"
+            ));
         }
-        let folder_name: &str = folder_name_chars.as_str();     // remove the leading c
+        let folder_name: &str = folder_name_chars.as_str(); // remove the leading c
         let channel_id: u64 = folder_name.parse()
             .map_err(|_| format!("{FOLDER_NAME_ERR_MSG} Folder name does start with 'c' but is not followed by a number: {folder_name}"))?;
 
-        channels.insert(channel_id, message_ids);
+        channels.insert(channel_id, messages);
     }
 
     Ok(channels)
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use crate::edit_message::edit_message;
-use crate::extract_message_ids::extract_message_ids;
+use crate::extract_message_ids::{Message, extract_messages};
 use crate::redact::{generate_redacted, generate_shakespeare};
 use colored::Colorize;
 use rand::random_range;
@@ -15,7 +15,7 @@ mod extract_message_ids;
 mod redact;
 mod wordlist;
 
-fn count_messages(channels: &HashMap<u64, Vec<u64>>) -> usize {
+fn count_messages(channels: &HashMap<u64, Vec<Message>>) -> usize {
     let mut count: usize = 0;
     for channel_messages in channels.values() {
         count += channel_messages.len();
@@ -24,67 +24,65 @@ fn count_messages(channels: &HashMap<u64, Vec<u64>>) -> usize {
 }
 
 fn main() -> Result<(), String> {
-    /* dotenv::dotenv().map_err(|e| format!("Could not initialize environment variables: {e}"))?;
-        let messages_directory: PathBuf = PathBuf::from(
-            std::env::var("DISCORD_MESSAGES_DIR")
-                .map_err(|_| "Environment variable DISCORD_MESSAGES_DIR not set")?,
-        );
-        let token: String =
-            std::env::var("DISCORD_TOKEN").map_err(|_| "Environment variable DISCORD_TOKEN not set")?;
+    dotenv::dotenv().map_err(|e| format!("Could not initialize environment variables: {e}"))?;
+    let messages_directory: PathBuf = PathBuf::from(
+        std::env::var("DISCORD_MESSAGES_DIR")
+            .map_err(|_| "Environment variable DISCORD_MESSAGES_DIR not set")?,
+    );
+    let token: String =
+        std::env::var("DISCORD_TOKEN").map_err(|_| "Environment variable DISCORD_TOKEN not set")?;
 
-        let channels: HashMap<u64, Vec<u64>> = extract_message_ids(&messages_directory)?;
-        println!(
-            "{}",
-            format!(
-                "Got {} messages in {} channels.\n",
-                count_messages(&channels),
-                channels.len()
-            )
-            .bright_purple()
-        );
+    let channels: HashMap<u64, Vec<Message>> = extract_messages(&messages_directory)?;
+    println!(
+        "{}",
+        format!(
+            "Got {} messages in {} channels.\n",
+            count_messages(&channels),
+            channels.len()
+        )
+        .bright_purple()
+    );
 
-        let client: Client = Client::builder()
+    let client: Client = Client::builder()
             .user_agent("Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
             .build()
             .map_err(|e| format!("Could not create reqwest client: {e}"))?;
-    */
 
     let shakespeare_mode = std::env::args().nth(1) == Some(String::from("shakespeare"));
 
     const QUOTES: &[u8] = include_bytes!("shakespeare.txt");
 
-    loop {
-        let length = random_range(0..2000);
-        println!("Generating quote for length {}", length);
-        let quote = generate_shakespeare(length, QUOTES);
-        println!("Thy generate quote shall be: \n{}", quote);
-    }
+    for (channel_id, messages) in channels {
+        for message in messages {
+            loop {
+                sleep(Duration::from_millis(5500));
+                println!(
+                    "Redacting message {} in channel {}.",
+                    message.ID.to_string().yellow(),
+                    channel_id.to_string().yellow()
+                );
+                let redacted_message: String;
 
-    /*
-        for (channel_id, message_ids) in &channels {
-            for message_id in message_ids {
-                loop {
-                    sleep(Duration::from_millis(5500));
-                    println!(
-                        "Redacting message {} in channel {}.",
-                        message_id.to_string().yellow(),
-                        channel_id.to_string().yellow()
-                    );
-                    let redacted_message: String = generate_redacted();
-                    match edit_message(&client, &token, *channel_id, *message_id, &redacted_message) {
-                        Ok(_) => break,
-                        Err((e, retry_after)) => {
-                            println!("{}", format!("Error while editing message: {e}").red());
-                            if retry_after == 0.0 {
-                                break;
-                            }
-                            sleep(Duration::from_secs_f64(retry_after * 1.2));
-                        }
-                    };
+                if shakespeare_mode {
+                    println!("Running shakespeare_mode");
+                    redacted_message = generate_shakespeare(message.Contents.len(), QUOTES);
+                } else {
+                    redacted_message = generate_redacted();
                 }
+
+                match edit_message(&client, &token, channel_id, message.ID, &redacted_message) {
+                    Ok(_) => break,
+                    Err((e, retry_after)) => {
+                        println!("{}", format!("Error while editing message: {e}").red());
+                        if retry_after == 0.0 {
+                            break;
+                        }
+                        sleep(Duration::from_secs_f64(retry_after * 1.2));
+                    }
+                };
             }
         }
-    */
+    }
 
     println!("{}", "\nDone!".bright_green());
 
